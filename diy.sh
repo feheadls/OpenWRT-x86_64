5cec588c5d31e3c00246282714fad31b234f407c
feheadls <45329238+feheadls@users.noreply.github.com>
2020-02-14 00:11:22 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,7 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.254/g' package/base-files/files/bin/config_generate
+echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
+git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan.git
+./scripts/feeds update -a
+./scripts/feeds install -a
